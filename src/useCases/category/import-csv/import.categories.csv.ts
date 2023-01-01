c8866a9b19c84c553ea5c9269a6fd47fb8b8e7d3
V3ronez <v3ronez.dev@gmail.com>
2023-01-01 20:38:39 -0300

    promise stream csv file

--- src/useCases/category/import-csv/import.categories.csv.ts
+++ src/useCases/category/import-csv/import.categories.csv.ts
@@ -1,5 +1,39 @@
+import fs from 'node:fs';
+import { parse } from 'csv-parse';
+import { ICategoryRepository } from '../../../repositories/category/category.repository.interface';
+
+interface IImportCategories {
+  name: string;
+  description: string;
+}
 export class ImportCategoriesCSVUseCase {
-  async execute(file: any) {
-    console.log(file);
+  constructor(private categoriesRepository: ICategoryRepository) {}
+
+  async LoadCategories(
+    file: Express.Multer.File,
+  ): Promise<IImportCategories[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategories[] = [];
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({ name, description });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(console.log(err));
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.LoadCategories(file);
+    console.log(categories);
   }
 }
--- src/useCases/category/import-csv/index.ts
+++ src/useCases/category/import-csv/index.ts
@@ -1,7 +1,11 @@
+import { CategoriesRepository } from '../../../repositories/category/category.repository';
 import { ImportCategoriesCSVUseCase } from './import.categories.csv';
 import { ImportCategoriesCSVController } from './import.categories.csv.controller';
 
-const importCategoriesCSVUseCase = new ImportCategoriesCSVUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoriesCSVUseCase = new ImportCategoriesCSVUseCase(
+  categoriesRepository,
+);
 const importCategoriesCSVController = new ImportCategoriesCSVController(
   importCategoriesCSVUseCase,
 );
