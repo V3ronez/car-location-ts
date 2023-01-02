2b1c9df323249d1accd93588e3e1ec6e4a8bd45f
V3ronez <v3ronez.dev@gmail.com>
2023-01-01 23:29:56 -0300

    start swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConfig from './swagger.json';
 import { routers } from './routes';
 
 const app = express();
-
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use(routers);
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server running in port ${PORT}  ⚡️`));
